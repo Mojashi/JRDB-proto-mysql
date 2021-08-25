04848179c50920249c92cad2f95de04e43dd7400
Omori Akihiro <mojashi@debian.debian>
2021-08-26 03:49:09 +0900

    impl translate

--- jrdb/model/schema.py
+++ jrdb/model/schema.py
@@ -1,8 +1,8 @@
-from typing import List, Type
-
+from typing import Dict, List, Type
 
 class Field:
     name : str
+    translatedName : str
     occ : int
     size : str
     docType : str
@@ -17,10 +17,11 @@ class Field:
         self.pos = pos
         self.comment = comment
 
+        self.translatedName = translate(name)
         self.pyType = str if docType[0] == 'X' else int
 
     def genProtoField(self, num:int) -> str:
-        return "\t%s %s = %d;" % ("string" if self.pyType == str else "int32", self.name, num)
+        return "\t%s %s = %d;" % ("string" if self.pyType == str else "int32", self.translatedName, num)
 
 class DataType:
     dtname : str
@@ -33,3 +34,18 @@ class DataType:
     def genProto(self) -> str:
         fields = "\n".join(map(lambda ifield: ifield[1].genProtoField(ifield[0] + 1), enumerate(self.fields)))
         return 'syntax = "proto3";\nmessage SearchRequest {\n%s\n}' % fields
+
+import pykakasi
+kks = pykakasi.kakasi()
+
+
+prechar = {'(':'かっこ', ')':'かっこ'}
+def translate(jp:str, endict:Dict[str,str] = {
+    '1':'one','2':'two','3':'three','4':'four','5':'five','6':'sic','7':'seven', '8':'eight','9':'nine','0':'zero','10':'ten',
+    '１':'one','２':'two','３':'three','４':'four','５':'five','６':'six','７':'seven', '８':'eight','９':'nine','０':'zero','１０':'ten',
+    '順位':'rank','◎':'perfect','○':'great','▲':'soso','△':'bad','コード':'code','３Ｆ':'three_F'
+}) -> str:
+    for o,n in prechar.items():
+        jp = jp.replace(o,n)
+
+    return "_".join(map(lambda item: endict[item['orig']] if item['orig'] in endict.keys() else item['hepburn'], kks.convert(jp)))
