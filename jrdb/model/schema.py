52071725e03828c35a169aa6e31f42eaa7c4b018
Omori Akihiro <mojashi@debian.debian>
2021-08-26 01:14:40 +0900

    impl genproto

--- jrdb/model/schema.py
+++ jrdb/model/schema.py
@@ -3,13 +3,33 @@ from typing import List, Type
 
 class Field:
     name : str
+    occ : int
+    size : str
     docType : str
     pyType : Type
-    
+    pos:int
+    comment:str
+    def __init__(self, name:str, occ:int, size:int, docType:str, pos:int, comment:str = "") -> None:
+        self.name = name
+        self.size = size
+        self.occ = occ
+        self.docType = docType
+        self.pos = pos
+        self.comment = comment
+
+        self.pyType = str if docType[0] == 'X' else int
+
+    def genProtoField(self, num:int) -> str:
+        return "\t%s %s = %d;" % ("string" if self.pyType == str else "int32", self.name, num)
 
 class DataType:
     dtname : str
     fields : List[Field]
 
+    def __init__(self, dtname:str) -> None:
+        self.dtname = dtname
+        self.fields = []
+
     def genProto(self) -> str:
-        pass
+        fields = "\n".join(map(lambda ifield: ifield[1].genProtoField(ifield[0] + 1), enumerate(self.fields)))
+        return 'syntax = "proto3";\nmessage SearchRequest {\n%s\n}' % fields
--- jrdb/parseDoc.py
+++ jrdb/parseDoc.py
@@ -3,7 +3,7 @@
 
 from typing import List
 from env import DocDir, ProtoDir
-from model.schema import DataType
+from model.schema import DataType, Field
 import logging
 import functools
 
@@ -17,14 +17,41 @@ def getBody(lines: List[str])->List[str]:
             astindex.append(idx)
     
     logging.debug(astindex)
-    assert(len(astindex) == 3)
     return lines[astindex[1] + 1:astindex[2]]
 
-def parseDoc(doc: str) -> DataType:
+def parseDoc(doc: str, dtname:str) -> DataType:
     body = getBody(doc.split("\n"))
-    logging.debug(body)
-    
-    return DataType()
+    dtype = DataType(dtname)
+
+    parent = ""
+    for line in body:
+        terms = line.split()
+
+        isChild = len(terms) > 0 and line[0].isspace()
+        isParent = len(terms) == 1 and not line[0].isspace()
+        haveOcc = len(terms) >= 5 and terms[1].isdigit() and terms[2].isdigit() and terms[4].isdigit()
+        isRecord = len(terms) >= 4 and (
+            haveOcc and terms[1].isdigit() and terms[2].isdigit() and terms[4].isdigit() or 
+            not haveOcc and terms[1].isdigit() and terms[3].isdigit())
+
+        logging.debug(line)
+        logging.debug(terms)
+        logging.debug([isRecord, isChild, isParent, haveOcc])
+
+        if isRecord:
+            if haveOcc:
+                field = Field(terms[0], int(terms[1]), int(terms[2]), terms[3], int(terms[4]), terms[5] if len(terms) >= 6 else "")
+            else:
+                field = Field(terms[0], 1, int(terms[1]), terms[2], int(terms[3]), terms[4] if len(terms) >= 5 else "")
+            if isChild:
+                field.name = parent+'_'+field.name
+
+            dtype.fields.append(field)
+
+        if isParent:
+            parent = terms[0]
+
+    return dtype
 
 
 import os
@@ -33,8 +60,8 @@ def parseAll(docDir:str = DocDir, protoDir : str = ProtoDir):
 
     for docfname in os.listdir(docDir):
         with open(docDir + "/" + docfname, "r") as f:
-            dtype = parseDoc(f.read())
-            with open(protoDir + "/" + dtype.dtname, "w") as protof:
+            dtype = parseDoc(f.read(), docfname)
+            with open(protoDir + "/" + dtype.dtname + ".proto", "w") as protof:
                 protof.write(dtype.genProto())
 
 
