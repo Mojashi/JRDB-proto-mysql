11800264351a09beffe09e3bf067578184ce4681
Mojashi <akihiro1001@gmail.com>
2021-09-12 21:42:58 +0900

    mod

--- parseData.py
+++ parseData.py
@@ -1,4 +1,5 @@
 #! python
+import threading
 from db.config import TableConfigs
 from google.protobuf import json_format
 from mysql.connector.cursor import CursorBase
@@ -9,8 +10,11 @@ from typing import IO, Any, List
 from env import DataDir, DtypeDescs, ProtoBuildDir
 from parseDoc import getDataType
 import logging
+import time
 import glob
 import sys
+import queue
+import itertools
 
 
 def parseData(ProtoT, dtype: DataType,
@@ -56,8 +60,8 @@ def parseData(ProtoT, dtype: DataType,
     return rows
 
 
-def parseAllData(cur: CursorBase, dtypeName: str,
-                 dataDir: str = DataDir, protoBuildDir: str = ProtoBuildDir):
+def insertAllData(cur: CursorBase, dtypeName: str,
+                  dataDir: str = DataDir, protoBuildDir: str = ProtoBuildDir):
     parentDtypeName = DtypeDescs[dtypeName.lower()].dataIncludedIn
     dir = getDtypeDataDir(parentDtypeName, dataDir)
 
@@ -76,29 +80,58 @@ def parseAllData(cur: CursorBase, dtypeName: str,
     conf = TableConfigs[dtypeName]
     ignore = "IGNORE" if conf.ignoreDuplicate else ""
 
-    rows = []
     files = sorted(glob.glob(dir + "/%s*.txt" % dtypeName.upper()))
 
-    for fname in files:
-        logging.info(fname)
-        with open(fname, "rb", 100000) as f:
-            rows.extend(parseData(ProtoT, dtype, fieldConvertors, f))
+    que: queue.Queue = queue.Queue(-1)
 
-        # print(len(values[0]), values[0])
-
-        if len(rows) > 10000:
+    def processQueue(que: queue.Queue, outputFile: str):
+        while True:
+            time.sleep(0.1)
+            cr: List[List[Any]] = []
+            try:
+                while True:
+                    cr.append(que.get_nowait())
+            except queue.Empty:
+                pass
+            if len(cr) == 0:
+                continue
+            logging.info(len(cr))
+            rows = itertools.chain.from_iterable(cr)
             try:
                 values = list(map(lambda row: insertConvFunc(row), rows))
-                holders = ",".join([r"%s" for _ in range(len(values[0]))])
-                cur.executemany(f"INSERT {ignore} INTO {dtypeName.capitalize()} "
-                                f"({columnList}) VALUES ({holders})", values)
-                del rows
-                rows = []
+                # holders = ",".join([r"%s" for _ in range(len(values[0]))])
+                # logging.info("a")
+                # cur.executemany(f"INSERT {ignore} INTO {dtypeName.capitalize()} "
+                #                 f"({columnList}) VALUES ({holders})", values)
+                with open(outputFile, "a") as f:
+                    for value in values:
+                        for elem in value:
+                            if type(elem) == bytes:
+                                f.write(elem.hex())
+                            elif elem is None:
+                                f.write(r"\N")
+                            else:
+                                f.write(str(elem))
+                            f.write("\t")
+                        f.write("\n")
+                logging.info("b")
             except Exception as e:
                 with open("dump.log", "w") as f:
                     for r in rows:
                         f.write(json_format.MessageToJson(r))
                 raise e
+
+    th = threading.Thread(target=processQueue,
+        args=(que, "/var/lib/mysql-files/loaddata.txt"), daemon=True)
+    th.start()
+
+    for fname in files:
+        logging.info(fname)
+        with open(fname, "rb", 100000) as f:
+            que.put(parseData(ProtoT, dtype, fieldConvertors, f))
+
+    cur.execute('LOAD DATA INFILE "/var/lib/mysql-files/loaddata.txt" %s'
+                'into table Cyb (%s) SET PROTO_BINARY=UNHEX(@var1);' % (ignore, columnList))
 
 
 def checkEmpty(cur: CursorBase, tableName: str) -> bool:
@@ -108,8 +141,7 @@ def checkEmpty(cur: CursorBase, tableName: str) -> bool:
     return True
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+def main():
     with getConn() as conn:
         conn.autocommit = False
         cur = conn.cursor()
@@ -118,14 +150,18 @@ if __name__ == "__main__":
             if sys.argv[1].lower() not in DtypeDescs.keys():
                 logging.error("unknown dtype")
                 exit(1)
-            parseAllData(cur, sys.argv[1])
+            insertAllData(cur, sys.argv[1])
             conn.commit()
         else:
             for dtypeName in DtypeDescs.keys():
                 if not checkEmpty(cur, dtypeName.capitalize()):
                     continue
-                parseAllData(cur, dtypeName)
+                insertAllData(cur, dtypeName)
                 conn.commit()
-    # parseAllData(cur, "srb")
-    # import cProfile
-    # cProfile.run('parseAllData("sed")', "prof")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    import cProfile
+    cProfile.run('main()', "prof")
+    exit(0)
